27bbeefbcc0288c8a94bd54c2250be252307366c
HenKyubi666 <jhonnino@unisangil.edu.co>
2022-09-09 16:59:26 -0500

    to deploy

--- config/database.js
+++ config/database.js
@@ -2,11 +2,17 @@ module.exports = ({ env }) => ({
   connection: {
     client: "postgres",
     connection: {
-      host: env("DATABASE_HOST", "127.0.0.1"),
+      host: env(
+        "DATABASE_HOST",
+        "postgres://ntlgldhciwcgvc:f314444384b8d2ac8338570f1c72a26f41921841582218183e04f0c23ff462ff@ec2-44-207-253-50.compute-1.amazonaws.com:5432/df07insph786lt"
+      ),
       port: env.int("DATABASE_PORT", 5432),
       database: env("DATABASE_NAME", "data-curso"),
-      user: env("DATABASE_USERNAME", "postgres"),
-      password: env("DATABASE_PASSWORD", "1234"),
+      user: env("DATABASE_USERNAME", "ntlgldhciwcgvc"),
+      password: env(
+        "DATABASE_PASSWORD",
+        "f314444384b8d2ac8338570f1c72a26f41921841582218183e04f0c23ff462ff"
+      ),
       ssl: env.bool("DATABASE_SSL", false),
     },
   },
